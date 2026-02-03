96f5e90956489703a92b7c59fd96e46850871b94
Paulina <paulina30405@gmail.com>
2026-02-03 21:28:44 +0100

    Created ActivatableMvvmWidget and ItemDetails widget, connected ItemDetails widget with VM data.

---
+++ Source/InventorySystem/Private/UI/Widgets/Inv_ItemDetails.cpp
@@ -0,0 +1,28 @@
+// Copyright Paulina Hałatek, All Rights Reserved.
+
+
+#include "UI/Widgets/Inv_ItemDetails.h"
+
+#include "Components/Image.h"
+#include "UI/ViewModels/SelectionViewModel.h"
+#include "UI/UIManagerSubsystem.h"
+#include "View/MVVMView.h"
+
+
+void UInv_ItemDetails::VM_SelectedItemImageUpdated(UTexture2D* Image)
+{
+	SelectedItemImage->SetBrushFromTexture(Image);
+}
+
+void UInv_ItemDetails::CacheViewModels(UUIManagerSubsystem* UIManager)
+{
+	MVVMView->SetViewModel("SelectionViewModel", UIManager->GetSelectionVM());
+	CachedSelectionVM = UIManager->GetSelectionVM();
+}
+
+void UInv_ItemDetails::ClearViewModelsCache()
+{
+	MVVMView->SetViewModel("SelectionViewModel", nullptr);
+	CachedSelectionVM = nullptr;
+}
+
