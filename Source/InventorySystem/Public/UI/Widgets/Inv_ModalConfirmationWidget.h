26aefd75b9e5be472f3af2592f314c7208bf638e
Paulina <paulina30405@gmail.com>
2026-02-04 19:01:56 +0100

    Created ModalConfirmationWidget

---
+++ Source/InventorySystem/Public/UI/Widgets/Inv_ModalConfirmationWidget.h
@@ -0,0 +1,16 @@
+﻿// Copyright Paulina Hałatek, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inv_ActivatableMvvmWidget.h"
+#include "Inv_ModalConfirmationWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInv_ModalConfirmationWidget : public UInv_ActivatableMvvmWidget
+{
+	GENERATED_BODY()
+};
