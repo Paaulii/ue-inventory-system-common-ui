7dee3a49dd6d82858eef5c107f3ce085bc3dc5cb
Paulina <paulina30405@gmail.com>
2026-02-04 14:13:38 +0100

    Added ItemActionButton class and added enabled and disabled animation. Added handling input both for input action and click interaction.

---
+++ Source/InventorySystem/Private/UI/Widgets/Inv_ItemActionButton.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright Paulina Hałatek, All Rights Reserved.
+
+
+#include "UI/Widgets/Inv_ItemActionButton.h"
+
+void UInv_ItemActionButton::SetHoveredState(bool bState)
+{
+	SetVisualStateWithAnimation(Hovered, bState);
+	
+}
+
+void UInv_ItemActionButton::SetEnabledState(bool bState)
+{
+	SetVisualStateWithAnimation(Disabled, bState);
+}
+
+void UInv_ItemActionButton::SetVisualStateWithAnimation(UWidgetAnimation* Animation, bool bPlayForward)
+{
+	EUMGSequencePlayMode::Type PlayModeType =  bPlayForward ? EUMGSequencePlayMode::Forward: EUMGSequencePlayMode::Reverse;
+	PlayAnimation(Animation,0,1,PlayModeType);
+}
