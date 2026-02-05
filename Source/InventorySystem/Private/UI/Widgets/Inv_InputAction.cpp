7bcf76137f2b559e0dbc96816a868a9d71d6c43b
Paulina <paulina30405@gmail.com>
2026-02-05 16:57:54 +0100

    Created InputActionPrompt class and connect it with widget.

---
+++ Source/InventorySystem/Private/UI/Widgets/Inv_InputAction.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright Paulina Hałatek, All Rights Reserved.
+
+
+#include "UI/Widgets/Inv_InputAction.h"
+#include "CommonActionWidget.h"
+
+void UInv_InputAction::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (!CustomInputAction.IsNull())
+	{
+		UpdateTriggeringInputAction(CustomInputAction);
+	}
+}
+
+void UInv_InputAction::UpdateTriggeringInputAction(const FDataTableRowHandle& InputAction)
+{
+	CustomInputAction = InputAction;
+	SetTriggeringInputAction(InputAction);
+	InputAction_CustomAction->SetInputAction(CustomInputAction);
+}
+
+void UInv_InputAction::HandleTriggeringActionCommited()
+{
+	Super::HandleTriggeringActionCommited();
+
+	if (IsInteractionEnabled())
+	{
+		OnTriggered.Broadcast();
+	}
+}
---
+++ Source/InventorySystem/Public/UI/Widgets/Inv_InputAction.h
@@ -0,0 +1,34 @@
+﻿// Copyright Paulina Hałatek, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "Inv_InputAction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInv_InputAction : public UCommonButtonBase
+{
+	GENERATED_BODY()
+public:
+	virtual void NativePreConstruct() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void UpdateTriggeringInputAction(const FDataTableRowHandle& InputAction);
+	
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnTriggered);
+	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	FOnTriggered OnTriggered;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom Input")
+	FDataTableRowHandle CustomInputAction;
+protected:
+	virtual void HandleTriggeringActionCommited() override;
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonActionWidget> InputAction_CustomAction;
+	
+};
