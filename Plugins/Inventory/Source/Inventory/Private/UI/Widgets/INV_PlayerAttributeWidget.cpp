2c02cf308e6c286645be4464d4da847741791e49
Paulina <paulina30405@gmail.com>
2026-02-13 21:45:42 +0100

    Created Player Attribute widget and added it to Character Details widget.

---
+++ Plugins/Inventory/Source/Inventory/Private/UI/Widgets/INV_PlayerAttributeWidget.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright Paulina Hałatek, All Rights Reserved.
+
+
+#include "UI/Widgets/INV_PlayerAttributeWidget.h"
+
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void UINV_PlayerAttributeWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	Image_StatIcon->SetBrushFromTexture(StatIcon);
+	Text_StatName->SetText(StatName);
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/UI/Widgets/INV_PlayerAttributeWidget.h
@@ -0,0 +1,35 @@
+﻿// Copyright Paulina Hałatek, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "INV_PlayerAttributeWidget.generated.h"
+
+class UTextBlock;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UINV_PlayerAttributeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere)
+	UTexture2D* StatIcon;
+	
+	UPROPERTY(EditAnywhere)
+	FText StatName;
+	
+protected:
+	virtual void NativePreConstruct() override;
+
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_StatIcon;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_StatName;
+};
