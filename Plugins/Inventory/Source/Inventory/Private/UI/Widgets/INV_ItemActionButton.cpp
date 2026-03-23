0695717f57396e524d937964b9eb23fcbc4a71ab
Paulina <paulina30405@gmail.com>
2026-03-23 13:12:21 +0100

    Moved ItemActionButton logic from Blueprints to c++

--- Plugins/Inventory/Source/Inventory/Private/UI/Widgets/INV_ItemActionButton.cpp
+++ Plugins/Inventory/Source/Inventory/Private/UI/Widgets/INV_ItemActionButton.cpp
@@ -3,6 +3,9 @@
 
 #include "UI/Widgets/INV_ItemActionButton.h"
 
+#include "CommonTextBlock.h"
+#include "UI/Widgets/INV_InputAction.h"
+
 void UINV_ItemActionButton::SetHoveredState(bool bState)
 {
 	SetVisualStateWithAnimation(Hovered, bState);
@@ -14,9 +17,25 @@ void UINV_ItemActionButton::SetEnabledState(bool bState)
 	SetVisualStateWithAnimation(Disabled, bState);
 }
 
+void UINV_ItemActionButton::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	SetButtonText(ButtonText);
+
+	if (Button_InputAction)
+	{
+		Button_InputAction->UpdateTriggeringInputAction(ClickInputAction);
+	}
+}
+
 void UINV_ItemActionButton::SetButtonVisibility(bool bState)
 {
 	SetVisibility(bState ? ESlateVisibility::Visible : ESlateVisibility::Collapsed);
+}
+
+void UINV_ItemActionButton::SetButtonText(const FText& NewText) const
+{
+	Text_Label->SetText(NewText);
 }
 
 void UINV_ItemActionButton::SetVisualStateWithAnimation(UWidgetAnimation* Animation, bool bPlayForward)
--- Plugins/Inventory/Source/Inventory/Public/UI/Widgets/INV_ItemActionButton.h
+++ Plugins/Inventory/Source/Inventory/Public/UI/Widgets/INV_ItemActionButton.h
@@ -6,6 +6,8 @@
 #include "CommonButtonBase.h"
 #include "INV_ItemActionButton.generated.h"
 
+class UINV_InputAction;
+class UCommonTextBlock;
 /**
  * 
  */
@@ -14,6 +16,7 @@ class INVENTORY_API UINV_ItemActionButton : public UCommonButtonBase
 {
 	GENERATED_BODY()
 public:
+	virtual void NativePreConstruct() override;
 	void SetButtonVisibility(bool bState);
 protected:
 	UFUNCTION(BlueprintCallable)
@@ -21,8 +24,22 @@ protected:
 	
 	UFUNCTION(BlueprintCallable)
 	void SetEnabledState(bool bState);
+
+	void SetButtonText(const FText& NewText) const;
+	
+	UPROPERTY(EditInstanceOnly, Category="Label")
+	FText ButtonText;
+	
+	UPROPERTY(EditInstanceOnly, Category = "Custom Input")
+	FDataTableRowHandle ClickInputAction;
 private:
 	void SetVisualStateWithAnimation(UWidgetAnimation* Animation, bool bPlayForward);
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UINV_InputAction> Button_InputAction;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> Text_Label;
 	
 	UPROPERTY(meta = (BindWidgetAnim), Transient)
 	TObjectPtr<UWidgetAnimation>  Hovered;
